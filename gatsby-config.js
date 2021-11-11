ca3b66bfc294f2a6f0a90411b7d219285a2ca9b4
Martez Howard <conner.martez@gmail.com>
2021-11-10 21:25:24 -0800

    update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Martez Howard`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `I have knowledge in many development technologies, my wide ranged knowledge and skill set is focused on full stack development including highly popular libraries such as react and react native. My knowledge and expertise spans across various libraries and technologies.`,
     author: `Martez Howard`,
   },
   plugins: [
